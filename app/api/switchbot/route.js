c37332d621fcaa81a92998ca53047aa46efecc67
Vincent Choy <vincent.choy07@gmail.com>
2025-03-30 10:15:09 +0800

    Refactor: use env variables for SwitchBot token and secret

--- app/api/switchbot/route.js
+++ app/api/switchbot/route.js
@@ -1,8 +1,12 @@
 import { NextResponse } from 'next/server';
 import crypto from 'crypto';
 
-const TOKEN = 'b7789a3db5007fe082e2eb2447a44a17b867525e5390fe081f10aec10e46ae4bad6667b1d079869723af2b6aa9277065';
-const SECRET = 'ab85ede58a0c95aa6b3a1da450d3c26e';
+//const TOKEN = 'b7789a3db5007fe082e2eb2447a44a17b867525e5390fe081f10aec10e46ae4bad6667b1d079869723af2b6aa9277065';
+//const SECRET = 'ab85ede58a0c95aa6b3a1da450d3c26e';
+
+const TOKEN = process.env.SWITCHBOT_TOKEN;
+const SECRET = process.env.SWITCHBOT_SECRET;
+
 
 function generateHeaders() {
   const nonce = crypto.randomUUID();
